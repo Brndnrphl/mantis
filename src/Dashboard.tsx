79333bf46c83a3b2777c165774e0dce7e725ce89
brndnrphl <brndnrphl@gmail.com>
2023-12-28 00:31:38 +0700

    Refactor Dashboard component and IconButton component
    The commit message suggests that the developer has made changes to the Dashboard component and IconButton component, specifically refactoring the code.

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,20 +1,26 @@
 import React from "react";
 import IconButton from "./components/iconButton";
-import { FaPlus, FaTrashAlt } from "react-icons/fa";
+import { FaTrashAlt } from "react-icons/fa";
+import { FaPlus } from "react-icons/fa6";
 
 const Dashboard = () => {
   return (
     <>
       <div className="flex flex-row items-start place-content-between p-1">
-        <h1 className="font-bold text-3xl mb-4">Notes</h1>
-        <div className="flex flex-row space-x-4">
-          <IconButton icon={FaPlus} bgColor="bg-black" />
+        <div className="flex flex-row space-x-4 justify-start">
+          <h1 className="font-bold text-3xl mb-4">Notes</h1>
           <IconButton
-            label="Delete Note"
-            icon={FaTrashAlt}
-            bgColor="bg-red-600"
+            icon={FaPlus}
+            bgColor="bg-white"
+            textColor="black"
+            className="border-[1px] border-slate-400"
           />
         </div>
+        <IconButton
+          label="Delete Note"
+          icon={FaTrashAlt}
+          bgColor="bg-red-600"
+        />
       </div>
     </>
   );
--- src/components/iconButton.tsx
+++ src/components/iconButton.tsx
@@ -4,17 +4,21 @@ interface IconButtonProps {
   label?: string;
   icon: IconType;
   bgColor: string;
+  textColor?: string;
+  className?: string;
 }
 
 const IconButton: React.FC<IconButtonProps> = ({
   label,
   icon: Icon,
   bgColor,
+  textColor = "text-white",
+  className,
 }) => {
   return (
     <button
       type="button"
-      className={`${bgColor} text-white py-2 px-3 flex items-center space-x-2 rounded-md hover:-translate-y-[0.15rem] transition-all ease-in-out`}
+      className={`${bgColor} ${textColor} py-2 px-3 flex items-center space-x-2 rounded-md hover:-translate-y-[0.15rem] transition-all ease-in-out ${className}`}
     >
       <Icon />
       {label && <span>{label}</span>}
