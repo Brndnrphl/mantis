71ea67967b17de37ebd77054b96176a3811f6b37
brndnrphl <brndnrphl@gmail.com>
2023-12-30 22:31:53 +0700

    Refactor NotePage component to fetch note data from API
    This commit refactors the NotePage component to fetch note data from the API instead of using the NoteContext. It also updates the loading state and renders a loading spinner while fetching the data. The fetched note content is now parsed using the marked library and displayed in the component.

--- src/NotePage.tsx
+++ src/NotePage.tsx
@@ -1,29 +1,49 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { useNotes } from "./NoteContext";
 import { marked } from "marked";
 import { ColorRing } from "react-loader-spinner";
 import parse from "html-react-parser";
 
 const NotePage = () => {
   const { noteId } = useParams();
-  const { note, fetchNote } = useNotes();
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [markdown, setMarkdown] = useState("");
+  const [title, setTitle] = useState("");
+
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(`/api/notes/${noteId}`);
+      const fetchedNote = await response.json();
+      setMarkdown(fetchedNote.content);
+      setTitle(fetchedNote.title);
+    } catch (error) {
+      console.error("Error fetching note:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
-    const fetchNoteData = async () => {
-      fetchNote(noteId);
-      setMarkdown(await marked.parse(note?.content));
-      setLoading(false);
-    };
-    fetchNoteData();
-  }, [noteId, fetchNote]);
+    fetchData();
+  }, []);
 
-  return (
+  return loading ? (
+    <>
+      <ColorRing
+        visible={true}
+        height="80"
+        width="80"
+        ariaLabel="blocks-loading"
+        wrapperStyle={{}}
+        wrapperClass="blocks-wrapper"
+        colors={["#000000", "#000000", "#000000", "#000000", "#000000"]}
+      />
+    </>
+  ) : (
     <>
       <h1 className="font-bold text-3xl mb-4 border-b-[1px] border-b-gray-300 p-2">
-        {note.title}
+        {title}
       </h1>
       <p
         className="
@@ -34,42 +54,11 @@ const NotePage = () => {
           prose-pre:rounded-lg
           prose-pre:p-4
           prose-code:font-normal"
-        dangerouslySetInnerHTML={{ __html: markdown }}
-      />
+      >
+        {parse(marked(markdown))}
+      </p>
     </>
   );
 };
 
 export default NotePage;
-
-// return isLoading ? (
-//   <>
-//     <ColorRing
-//       visible={true}
-//       height="80"
-//       width="80"
-//       ariaLabel="blocks-loading"
-//       wrapperStyle={{}}
-//       wrapperClass="blocks-wrapper"
-//       colors={["#000000", "#000000", "#000000", "#000000", "#000000"]}
-//     />
-//   </>
-// ) : (
-//   <>
-//     <h1 className="font-bold text-3xl mb-4 border-b-[1px] border-b-gray-300 p-2">
-//       {note?.title}
-//     </h1>
-//     <p
-//       className="
-//         text-md mb-2
-//         prose
-//         prose-indigo
-//         max-w-none
-//         prose-pre:rounded-lg
-//         prose-pre:p-4
-//         prose-code:font-normal"
-//       // biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation>
-//       dangerouslySetInnerHTML={{ __html: markdown }}
-//     />
-//   </>
-// );
